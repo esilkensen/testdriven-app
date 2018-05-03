96b27831774ef0910fbadbf17918c236082be817
Erik Silkensen <silkense@colorado.edu>
2018-05-03 00:44:11 -0600

    Part 3, flake8

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -48,6 +48,7 @@ def register_user():
         db.session.rollback()
         return jsonify(response_object), 400
 
+
 @auth_blueprint.route('/auth/login', methods=['POST'])
 def login_user():
     # get post data
@@ -77,6 +78,7 @@ def login_user():
         response_object['message'] = 'Try again.'
         return jsonify(response_object), 500
 
+
 @auth_blueprint.route('/auth/logout', methods=['GET'])
 def logout_user():
     # get auth token
@@ -97,6 +99,7 @@ def logout_user():
             return jsonify(response_object), 401
     else:
         return jsonify(response_object), 403
+
 
 @auth_blueprint.route('/auth/status', methods=['GET'])
 def get_user_status():
--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -2,8 +2,6 @@ import json
 
 from flask import current_app
 
-from project import db
-from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
